1eee30b90406166d3573b99ec97f1ecfdaa88cce
Destructor266 <jarmastertruck@gmail.com>
2022-06-23 19:17:42 +0200

    Hello World

--- build.sh
+++ build.sh
@@ -30,44 +30,44 @@ makepkg -si
 
 sudo pacman -S - < $PWD/Downloads/ArchConf/pkglist.txt
 
-cp $PWD/Downloads/ArchConf/fehbg/.fehbg $PWD/
+cp $PWD/Downloads/ArchConf/fehbg/.fehbg $HOME
 
 cd $PWD/.config
 
 if ! [[ -d $PWD/.config/alacritty ]]
 then
 	mkdir $PWD/.config/alacritty
-	cp $PWD/Downloads/ArchConf/alacritty/alacritty.yml $PWD/.config/alacritty/
+	cp $PWD/Downloads/ArchConf/alacritty/alacritty.yml $HOME/.config/alacritty/
 else
-	cp $PWD/Downloads/ArchConf/alacritty/alacritty.yml $PWD/.config/alacritty
+	cp $PWD/Downloads/ArchConf/alacritty/alacritty.yml $HOME/.config/alacritty
 fi
 
 if ! [[ -d $PWD/.config/i3 ]]
 then
 	mkdir $PWD/.config/i3
-	cp $PWD/Downloads/ArchConf/i3/config $PWD/.config/i3/
-	cp $PWD/Downloads/ArchConf/i3/i3status.conf $PWD/.config/i3/
+	cp $PWD/Downloads/ArchConf/i3/config $HOME/.config/i3/
+	cp $PWD/Downloads/ArchConf/i3/i3status.conf $HOME/.config/i3/
 else
-	cp $PWD/Downloads/ArchConf/i3/config $PWD/.config/i3/
-	cp $PWD/Downloads/ArchConf/i3/i3status.conf $PWD/.config/i3/
+	cp $PWD/Downloads/ArchConf/i3/config $HOME/.config/i3/
+	cp $PWD/Downloads/ArchConf/i3/i3status.conf $HOME/.config/i3/
 fi
 
 if ! [[ -d $PWD/.config/nvim ]]
 then
 	mkdir $PWD/.config/nvim
-	cp $PWD/Downloads/ArchConf/nvim/init.vim $PWD/.config/nvim/
+	cp $PWD/Downloads/ArchConf/nvim/init.vim $HOME/.config/nvim/
 	nvim -c "PlugInstall"
 else
-	cp $PWD/ArchConf/nvim/init.vim $PWD/.config/nvim/
+	cp $PWD/ArchConf/nvim/init.vim $HOME/.config/nvim/
 	
 fi
 
 if ! [[ -d $PWD/.config/picom ]]
 then
 	mkdir $PWD/.config/nvim
-	cp $PWD/Downloads/ArchConf/picom/picom.conf $PWD/.config/picom/
+	cp $PWD/Downloads/ArchConf/picom/picom.conf $HOME/.config/picom/
 else
-	cp $PWD/Downloads/ArchConf/picom/picom.conf $PWD/.config/picom/
+	cp $PWD/Downloads/ArchConf/picom/picom.conf $HOME/.config/picom/
 fi
 
 
