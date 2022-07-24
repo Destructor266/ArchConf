30f07fe2f2cec8886bdb08c622aa9f54c3f11c31
Destructor266 <jarmastertruck@gmail.com>
2022-07-24 21:49:11 +0200

    Commented an install command

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ else
 	cp $PWD/fonts/* $HOME/.fonts/
 fi 
 
-sudo pacman -S --needed base-devel
+#sudo pacman -S --needed base-devel
 
 git clone https://aur.archlinux.org/paru.git $HOME/Downloads/paru
 cd $HOME/Downloads/paru
